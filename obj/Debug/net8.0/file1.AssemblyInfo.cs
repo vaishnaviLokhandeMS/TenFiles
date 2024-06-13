e2e00df67b17772f98b71b4ea8c4d9fe312a3d22
vaishnaviLokhandeMS <lokhande.vaishnavi@outlook.com>
2024-06-13 12:28:22 +0530

    first commit

--- obj/Debug/net8.0/file1.AssemblyInfo.cs
+++ obj/Debug/net8.0/file1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("file1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd02696ffb727e2aaf2cf3b8af48667bf9646c8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("file1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("file1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
